a13bb565e53bc98c59849517256994cadc737966
AllaJohnsson <deep_ampik@bk.ru>
2017-03-29 16:27:43 -0400

    Add episodes MCV

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :configure_permited_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password,
+      :password_confirmation, :current_password, :title)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password,
+      :password_confirmation, :current_password, :title, :description, :itunes, :stitcher, :podbay)}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
   devise_for :podcasts
-  root 'welcome#index'
-  get 'podcasts', to: 'podcasts#podcasts'
-  get 'podcasts/show', to: 'podcasts#show'
-  get 'podcasts/dashboard', to: 'podcasts#dashboard'
-  get 'podcasts/episode', to: 'podcasts#episode'
-  get 'welcome/sign_in', to: 'welcome#sign_in'
-  get 'welcome/sign_up', to: 'welcome#sign_up'
+
+  resources :podcasts, only: [:index, :show] do
+     resources :episodes
+  end
+
+   authenticated :podcast do
+       root 'podcasts#dashboard', as: "authenticated_root"
+   end
+
+   root 'welcome#index'
+  # get 'podcasts', to: 'podcasts#podcasts'
+  # get 'podcasts/show', to: 'podcasts#show'
+  # get 'podcasts/dashboard', to: 'podcasts#dashboard'
+  # get 'podcasts/episode', to: 'podcasts#episode'
+  # get 'welcome/sign_in', to: 'welcome#sign_in'
+  # get 'welcome/sign_up', to: 'welcome#sign_up'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
