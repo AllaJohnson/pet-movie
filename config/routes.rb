66c94c2cd216f10c1e8961a0bbfda00f19a0e3f9
AllaJohnsson <deep_ampik@bk.ru>
2017-04-04 00:08:55 -0400

    routes comented

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-   default_url_options :host => "locajhost:3000"
+   #default_url_options :host => "locajhost:3000"
   devise_for :podcasts
 
   resources :podcasts, only: [:index, :show] do
