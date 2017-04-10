61bb89a96ff8a35cf2a706e3c3b564be33b2d283
AllaJohnsson <deep_ampik@bk.ru>
2017-04-09 23:29:26 -0400

    Capistrano adding

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,6 @@ Rails.application.routes.draw do
    end
 
    root 'welcome#index'
-  # get 'podcasts', to: 'podcasts#podcasts'
-  # get 'podcasts/show', to: 'podcasts#show'
-  # get 'podcasts/dashboard', to: 'podcasts#dashboard'
-  # get 'podcasts/episode', to: 'podcasts#episode'
-  # get 'welcome/sign_in', to: 'welcome#sign_in'
-  # get 'welcome/sign_up', to: 'welcome#sign_up'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
