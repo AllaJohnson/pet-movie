cb8786626b0b7816fb3a540828ac35f4f001e330
AllaJohnsson <deep_ampik@bk.ru>
2017-04-14 22:05:16 -0400

    Renaming finish to deploy

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class PodcastsControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get podcasts_index_url
+    get users_index_url
     assert_response :success
   end
 
