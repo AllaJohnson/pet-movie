50f5b0c40cd8c086295cfd09d137161d89c06db9
AllaJohnsson <deep_ampik@bk.ru>
2017-04-27 12:47:32 -0400

    Capistrano install && Readding devise.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
   belongs_to :user
 
-  has_attached_file :movie_logo, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/s-missing.png"
+  has_attached_file :movie_logo, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/c-missing.png"
   validates_attachment_content_type :movie_logo, content_type: /\Aimage\/.*\z/
 
   #has_attached_file :mp3
@@ -9,7 +9,7 @@ class Movie < ApplicationRecord
 
   has_attached_file :video, :styles => {
     :medium => { :geometry => "640x480", :format => 'flv' },
-    :thumb => { :geometry => "100x100#", :format => 'jpg', :time => 10 }, default_url: "/images/:style/s-missing.png"
+    :thumb => { :geometry => "100x100#", :format => 'jpg', :time => 10 }, default_url: "/images/:style/c-missing.png"
   }, :processors => [:transcoder]
    validates_attachment_content_type :video, content_type: /\Avideo\/.*\Z/
 end
