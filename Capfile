3531f0a0d7f211be98e5351d5e268fdb58d2d80e
AllaJohnsson <deep_ampik@bk.ru>
2017-04-27 14:12:58 -0400

    Issue with Devise -treat1

--- Capfile
+++ Capfile
@@ -36,7 +36,7 @@ set :rbenv_ruby, '2.3.3'
 require "capistrano/bundler"
 require "capistrano/rails"
 # require "capistrano/rails/migrations"
-require "capistrano/passenger"
+# require "capistrano/passenger"
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-   #default_url_options :host => "locajhost:3000"
-  #devise_for :podcasts
 
   resources :users, only: [:index, :show] do
      resources :movies
@@ -12,11 +10,6 @@ Rails.application.routes.draw do
    end
 
    root 'welcome#index'
-  # get 'podcasts', to: 'podcasts#podcasts'
-  # get 'podcasts/show', to: 'podcasts#show'
-  # get 'podcasts/dashboard', to: 'podcasts#dashboard'
-  # get 'podcasts/episode', to: 'podcasts#episode'
-  # get 'welcome/sign_in', to: 'welcome#sign_in'
-  # get 'welcome/sign_up', to: 'welcome#sign_up'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
