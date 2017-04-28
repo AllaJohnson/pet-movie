05f41ccafdf1c100f4032a501f84eabde985b544
AllaJohnsson <deep_ampik@bk.ru>
2017-04-27 20:17:28 -0400

    remove :small from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :trackable, :validatable
    has_many :movies
 
-   has_attached_file :photo, styles: { medium: "300x300#", thumb: "100x100", small: "10x10" }, default_url: "/images/:style/c-missing.png"
+   has_attached_file :photo, styles: { medium: "300x300#", thumb: "100x100" }, default_url: "/images/:style/c-missing.png"
    validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
